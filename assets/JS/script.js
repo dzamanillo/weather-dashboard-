49ffbd5f1bf859a63536de0efb1ddf08a8c9da2f
Daniel Zamanillo <dzamanillok@gmail.com>
2021-07-17 16:31:43 -0500

    buttons are pulling info from api to build main section

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,26 +1,14 @@
 var saveCitiesEl = document.querySelector("#saved-cities");
 var searchBtnEl = document.querySelector("#search-btn");
 var searchAreaEl = document.querySelector("#city-search-area");
+var selectedCityEl = document.querySelector("#selected-city-stats");
+var cityName;
+var lat;
+var lon;
 
+console.log(selectedCityEl);
 // Get or create "cities" in local storage
 var cities = JSON.parse(localStorage.getItem("cities")) || [];
-
-//@ Search Button Adds Value to "cities" Local Storage
-searchBtnEl.addEventListener("click", function () {
-	saveCitiesEl.innerHTML = "";
-	console.log(searchAreaEl.value);
-
-	// Push protect
-	if (cities.indexOf(searchAreaEl.value) === -1) {
-		cities.push(searchAreaEl.value);
-	}
-
-	// Update localStorage after adding new city
-	localStorage.setItem("cities", JSON.stringify(cities));
-
-	// Build List from local
-	buildMenu();
-});
 
 //* Function Build buttons from cities local storage
 var buildMenu = function () {
@@ -35,16 +23,118 @@ var buildMenu = function () {
 	}
 };
 
+//* Remove Children Function
+function removeAllChildNodes(parent) {
+	while (parent.firstChild) {
+		parent.removeChild(parent.firstChild);
+	}
+}
+
 //* Get City Name Function
 var getCityName = function (event) {
 	if (event.target.dataset.btn === "city") {
-		var cityName = event.target.value;
-		console.log(cityName);
+		var clickedCityName = event.target.value;
+		console.log(clickedCityName);
+		cityName = clickedCityName;
 	}
 };
 
+//* City Fetch Function
+var cityFetch = function () {
+	var apiUrl =
+		"https://api.openweathermap.org/data/2.5/weather?q=" +
+		cityName +
+		"&units=imperial&appid=393222282915c98d946ff653251f7404";
+
+	fetch(apiUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+
+			removeAllChildNodes(selectedCityEl);
+
+			// City
+			var city = data.name;
+			var selectedCityTitle = document.createElement("h2");
+			selectedCityTitle.textContent = city;
+
+			// Temp
+			var temp = data.main.temp;
+			var selectedCityTemp = document.createElement("p");
+			selectedCityTemp.innerHTML = "Temp: " + temp;
+
+			// Wind
+			var wind = data.wind.speed;
+			var selectedCityWind = document.createElement("p");
+			selectedCityWind.innerHTML = "Wind: " + wind;
+
+			//Humidity
+			var humidity = data.main.humidity;
+			var selectedCityHumidity = document.createElement("p");
+			selectedCityHumidity.innerHTML = "Humidity: " + humidity;
+
+			// TODO UV Index
+			// var uvIndex = data.main.uvIndex;
+			var uvIndex = "To Do";
+			var selectedCityUvIndex = document.createElement("p");
+			selectedCityUvIndex.innerHTML = "UV Index: " + uvIndex;
+
+			lat = data.coord.lat;
+			lon = data.coord.lon;
+
+			console.log(lat, lon);
+
+			selectedCityEl.append(
+				selectedCityTitle,
+				selectedCityTemp,
+				selectedCityWind,
+				selectedCityHumidity,
+				selectedCityUvIndex
+			);
+		});
+};
+
+//* Saved City Button Handler Function
+var savedCityBtnHandler = function (event) {
+	getCityName(event);
+	cityFetch();
+};
+
+//@ Search Button Adds Value to "cities" Local Storage
+//@ Also builds main area
+searchBtnEl.addEventListener("click", function () {
+	if (!searchAreaEl.value) {
+		alert("Please Enter a City");
+		return;
+	} else {
+		saveCitiesEl.innerHTML = "";
+		console.log(searchAreaEl.value);
+
+		// Push protect
+		if (cities.indexOf(searchAreaEl.value) === -1) {
+			cities.push(searchAreaEl.value);
+		}
+
+		removeAllChildNodes(selectedCityEl);
+		cityName = searchAreaEl.value;
+		cityFetch();
+
+		// Update localStorage after adding new city
+		localStorage.setItem("cities", JSON.stringify(cities));
+
+		// Build List from local
+		buildMenu();
+		searchAreaEl.value = "";
+	}
+});
+
 //@ City Buttons
-saveCitiesEl.addEventListener("click", getCityName);
+saveCitiesEl.addEventListener("click", savedCityBtnHandler);
 
 //!On Load
 buildMenu();
+
+// API Key
+// 393222282915c98d946ff653251f7404
